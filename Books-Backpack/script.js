765cea2ad90a87f3d1d384ea6e4d3df250a851ee
EvrestRGB <elzadam11@gmail.com>
2023-06-21 19:33:06 +0100

    Summary

--- Books-Backpack/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("backpack").onclick = function() {
-    document.getElementById("books").style.display = "block";
-  };
-  
-  document.getElementById("books ul li a").onclick = function() {
-    var text = this.innerHTML;
-    alert(text);
-  };
-  
